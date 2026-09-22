ea21a79704d2375e0d04dd283eefb29e8dc0ad58
yanik404 <yanik@beckweb.ch>
2026-09-22 18:11:11 +0200

    Add game field

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
+#define SIZE 10
+
+void printLabyrinth(char labyrinth[SIZE][SIZE]);
+
 int main(void)
 {
-    printf("Labyrinth-Spiel\n");
+    char labyrinth[SIZE][SIZE] = {{' '}};
+
+    printf("=== Labyrinth-Spiel ===\n");
+    printLabyrinth(labyrinth);
     return 0;
+}
+
+void printLabyrinth(char labyrinth[SIZE][SIZE])
+{
+    int row, col;
+
+    for (row = 0; row < SIZE; row++)
+    {
+        for (col = 0; col < SIZE; col++)
+        {
+            printf(" %c ", labyrinth[row][col]);
+        }
+        printf("\n");
+    }
 }
